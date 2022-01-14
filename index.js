9b9a6b16de74aefed3a93e90daf3eaa033f50264
Tushar <tusharmagar816@gmil.com>
2022-01-14 21:43:59 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,124 @@
+const grid = document.querySelector('.grid')
+const startButton = document.getElementById('start')
+const scoreDisplay = document.getElementById('score')
+const restartButton = document.getElementById('restart')
+let squares =[]
+let currentSnake =[2,1,0]
+let direction = 1
+let walk;
+let width =10
+let appleIndex = 0
+let score = 0
+let timeInterval = 1000
+let timerId =0
+
+function createGrid() {
+    for (let i=0; i < 100; i++) {
+    const square = document.createElement('div')
+    console.log(square)
+    square.classList.add('square')
+    grid.appendChild(square)      
+    squares.push(square)
+    }
+}
+createGrid()
+
+currentSnake.forEach(function(index){
+    squares[index].classList.add('snake')
+})
+
+function startGame() {
+  
+       
+      
+       timerId = setInterval(move, timeInterval)
+}
+
+
+function restartGame(){
+
+
+    currentSnake.forEach(index => squares[index].classList.remove('snake'))
+    squares[appleIndex].classList.remove('apple')
+    clearInterval(timerId)
+    currentSnake = [2,1,0]
+    score = 0
+    scoreDisplay.textContent = score
+    direction = 1
+    timeInterval=1000
+    genrateApple()
+    currentSnake.forEach(index => squares[index].classList.add('snake'))
+    timerId = setInterval(move, timeInterval)
+
+}
+
+function move(){
+    if (
+        (currentSnake[0] + width >= width*width && direction === width) || 
+        (currentSnake[0] % width === width-1 && direction === 1) || 
+        (currentSnake[0] - width < 0 && direction === -width) || 
+        squares[currentSnake[0] + direction].classList.contains('snake')
+    )
+    return clearInterval(timerId)
+    
+    
+
+    const tail = currentSnake.pop()
+    
+    
+    squares[tail].classList.remove('snake')
+    currentSnake.unshift(currentSnake[0]+direction)
+
+
+    if (squares[currentSnake[0]].classList.contains('apple')) {
+        
+        squares[currentSnake[0]].classList.remove('apple')
+
+        squares[tail].classList.add('snake')
+        console.log(tail)
+        currentSnake.push(tail)
+        console.log(currentSnake)
+        genrateApple()
+        score++
+        scoreDisplay.textContent = score
+        clearInterval(timerId)
+        timeInterval = timeInterval * 0.8
+        setInterval(move ,timeInterval)
+    }
+    
+    squares[currentSnake[0]].classList.add('snake')
+
+}
+
+
+
+function genrateApple(){
+do {
+    appleIndex =Math.floor(Math.random()*squares.length)
+} while (squares[appleIndex].classList.contains('snake'))
+         (squares[appleIndex].classList.add('apple'))
+
+}
+genrateApple()
+
+document.addEventListener("keydown" ,function(event){
+    if(event.keyCode==37){
+        walk="left";
+        direction = 1
+    }else if(event.keyCode==38){
+        walk="top";
+        direction = -width
+    }else if(event.keyCode==39){
+        walk="right";
+        direction = -1
+    }else if(event.keyCode==40){
+        walk="down";
+        direction = width
+    }
+    console.log(walk);
+})
+
+
+
+startButton.addEventListener('click' ,startGame)
+restartButton.addEventListener('click' , restartGame)
